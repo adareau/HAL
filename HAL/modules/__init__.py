dee4ae32046ea41ad2fa0cea60cd35f48e4a73d8
Alexandre <alexandre.Dareau@gmail.com>
2021-06-09 17:57:26 +0200

    init in modules

---
+++ HAL/modules/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Author   : Alexandre
+Created  : 2021-06-09 10:21:17
+
+Comments :
+"""
+import pkgutil
+
+print("IMPORTING : MODULES")
+print(__path__)
+
+__all__ = []
+loaded_modules = []
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    print(module_name)
+    __all__.append(module_name)
+    _module = loader.find_module(module_name).load_module(module_name)
+    globals()[module_name] = _module
+    loaded_modules.append(module_name)
